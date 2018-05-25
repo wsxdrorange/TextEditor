ace234c8fc0a5d7c727fd652ee157adff1ff9417
wsxdrorange <zhang.rick00@gmail.com>
2018-05-25 15:24:14 -0500

    Finish pgdown,pgup,del, home,end keys

--- kilo.c
+++ kilo.c
@@ -14,7 +14,12 @@ enum editorKey{
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN,
+    HOME_KEY,
+    END_KEY,
+    DEL_KEY
 };
 
 //Global Variable Declaration
@@ -76,14 +81,37 @@ int editorReadKey(){ //Read in key
         if(read(STDIN_FILENO, &seq[1], 1) != 1)return '\x1b';
 
         if(seq[0] == '['){
-            switch(seq[1]){
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+            if(seq[1] >= '0' && seq[1] <= '9'){
+                if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if(seq[2] == '~'){
+                    switch(seq[1]){
+                        case '1': return HOME_KEY;
+                        case '2': return DEL_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                    }
+                }
+            }
+            else{
+                switch(seq[1]){
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                }
             }
         }
-
+        else if(seq[0] == '0'){
+            switch(seq[1]){
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+            }
+        }
         return '\x1b';
     }
     else{
@@ -186,16 +214,24 @@ void editorRefreshScreen(){ //Positions cursor and clears contents
 void editorMoveCursor(int key){
     switch(key){
         case ARROW_LEFT:
-            E.cx--;
+            if (E.cx != 0){
+                E.cx--;
+            }
             break;
         case ARROW_RIGHT:
-            E.cx++;
+            if (E.cx != E.screencols-1){
+                E.cx++;
+            }
             break;
         case ARROW_UP:
-            E.cy--;
+            if (E.cy != 0){
+                E.cy--;
+            }
             break;
         case ARROW_DOWN:
-            E.cy++;
+            if (E.cy != E.screenrows -1){
+                E.cy++;
+            }
             break;
     }
 }
@@ -207,7 +243,19 @@ void editorProcessKeypress(){
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
-    
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols-1;
+            break;
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenrows;
+                while(times--) editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+            break;
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_LEFT:
